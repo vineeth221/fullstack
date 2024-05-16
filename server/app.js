78d38af4fadd1d72a08772a7bcc45ba9fb95b0a0
apple <apple@apples-MacBook-Pro.local>
2024-05-16 19:37:41 +0530

    message

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const router = require("./routes/router");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // Middleware
 app.use(express.json());
