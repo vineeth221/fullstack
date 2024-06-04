80a283653a18c3ccec529181ec8e25b168ab6b46
apple <apple@apples-MacBook-Pro.local>
2024-06-04 19:10:50 +0530

    my styles

--- client/src/components/landing/WhatsAppIcon.js
+++ client/src/components/landing/WhatsAppIcon.js
@@ -6,6 +6,7 @@ const WhatsAppIcon = () => {
     return (
         <a href="https://wa.me/7411283407" className="whatsapp-float" target="_blank" rel="noopener noreferrer">
             <FaWhatsapp className="whatsapp-icon" />
+            <span class="tawk-badge tawk-flex tawk-flex-center tawk-flex-middle tawk-min-badge" style={{inset: "auto auto 35px 39px"}}>1</span>
         </a>
     );
 };
